22dddee43b05a10455f86c28aa21401553d0a1fe
erikwski <ferrarinumero10@gmail.com>
2021-11-07 02:42:34 +0100

    Gestito checkout da terminare

--- public/js/home.js
+++ public/js/home.js
@@ -80,20 +80,3 @@ btn_show_stats.addEventListener("click", () => {
     setIntervalStats(document.getElementById("stat3"), 280);
   }
 });
-
-// var swiper = new Swiper(".mySwiper", {
-//   effect: "coverflow",
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: "auto",
-//   coverflowEffect: {
-//     rotate: 50,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 1,
-//     slideShadows: true,
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-// });
