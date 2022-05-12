a3d0951f211859bd79518f25037f48c3ddc71cd1
erikwski <ferrarinumero10@gmail.com>
2022-05-12 20:10:37 +0200

    Sistemate cartelle progetto e iniziata landing

---
+++ CESTINO OLD PAGE/ALEXANDER SERRA DEFINITIVO/public/js/index.js
@@ -0,0 +1,16 @@
+// setTimeout(() => {
+//   //OMINO ARRIVA AL CENTRO DELLO SCHERMO
+//   $("#loader").addClass("runner_on_center");
+//   setTimeout(() => {
+//     //TESTO DEL LOGO ARRIVA AL CENTRO
+//     $("#loader").addClass("text_on_center");
+//     setTimeout(() => {
+//       //SIA TESTO CHE OMINO CORRONO OUT
+//       $("#loader").addClass("logo_out");
+//       setTimeout(() => {
+//BACKGROUND ESCE DALLO SCHERMO
+$("html").addClass("loaded");
+//       }, 900);
+//     }, 1000);
+//   }, 1200);
+// }, 500);
