602060f23a06dccee605a63f954b0e972f83fd3a
Erik Ferrari <eferrari@datacolor.com>
2024-08-12 12:17:53 +0200

    navigateBackDefault

--- js/indexPercorso.js
+++ js/indexPercorso.js
@@ -6,25 +6,13 @@ $(function () {
     $(".site-wrapper").addClass("navigationIn");
     $("#openingPercorso").addClass("loaded");
     $("html").addClass("loaded enable_scroll");
-    //push new state for prevent back
-    window.addEventListener("hashchange", navigateBack);
-    window.addEventListener("popstate", navigateBack);
-    
-    $(".navigateBack").on("click", navigateBack);
-  });
 
-  function navigateBack(e){
-    console.log(e);
-    if(e){
-      e.stopPropagation();
-      e.preventDefault();
-    }
-    console.log("back");
-    $("#openingPercorso").removeClass("loaded");
-    $(".site-wrapper").removeClass("navigationIn");
-    debugger;
-    setTimeout(() => {
-      window.history.back();
-    }, 500);
-  }
+    $(".navigateBack").on("click", ()=>{
+      $("#openingPercorso").removeClass("loaded");
+      $(".site-wrapper").removeClass("navigationIn");
+      setTimeout(() => {
+        window.history.back();
+      }, 500);
+    });
+  });
 });
