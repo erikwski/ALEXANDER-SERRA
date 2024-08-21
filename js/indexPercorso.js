e803d730aec7e60dcc43de7ac6ec2a707cf50d40
Erik Ferrari <eferrari@datacolor.com>
2024-08-21 22:51:39 +0200

    Commit for save actual edit

--- js/indexPercorso.js
+++ js/indexPercorso.js
@@ -8,6 +8,8 @@ $(function () {
     $("html").addClass("loaded enable_scroll");
   
     $(".navigateBack").on("click", navigateBack);
+
+    slowScroll();
   });
 
   function navigateBack(e){
@@ -16,5 +18,33 @@ $(function () {
     setTimeout(() => {
       // window.history.back();
     }, 500);
+  }
+
+  function slowScroll() {
+    $(
+      'a.button'
+    ).on("click", function (e) {
+      if ($(this).attr("href") === "#") {
+        e.preventDefault();
+      } else {
+        if ($(window).width() < 1024) {
+          if (!$(e.target).is(".sub-arrow")) {
+            $("html, body").animate(
+              { scrollTop: $(this.hash).offset().top },
+              500
+            );
+            $(".menu-holder").removeClass("show");
+            $("#toggle").removeClass("on");
+            return false;
+          }
+        } else {
+          $("html, body").animate(
+            { scrollTop: $(this.hash).offset().top },
+            500
+          );
+          return false;
+        }
+      }
+    });
   }
 });
